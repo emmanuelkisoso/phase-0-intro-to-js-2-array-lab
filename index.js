87983b5f4d5e50704d3b5bfcfd37fc9120b91713
emmanuel <emmanuel.kisoso@student.moringaschool.com>
2024-03-25 10:41:43 +0300

    Intial Commit

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+let cats=["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph')
+}
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat() {
+    cats.pop('Garfield')
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift('Milo')
+}
+function appendCat(name) {
+    let copyOfCats=cats.slice();
+    copyOfCats.push('Broom');
+    return copyOfCats;
+}
+function prependCat(name) {
+    let copyOfCats=cats.slice();
+    copyOfCats.unshift('Arnold');
+    return copyOfCats;
+}
+function removeLastCat(name) {
+    let copyOfCats=cats.slice();
+    copyOfCats.pop('Garfield');
+    return copyOfCats;
+}
+function removeFirstCat(name) {
+    let copyOfCats=cats.slice();
+    copyOfCats.shift('Milo');
+    return copyOfCats;
+}
